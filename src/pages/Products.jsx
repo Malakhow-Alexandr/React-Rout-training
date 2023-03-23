4661a4e882e9e63200acc3dc52b09d5ef056fab6
Malakhow-Alexandr <109951660+Malakhow-Alexandr@users.noreply.github.com>
2023-03-23 11:11:11 +0200

    add example

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,28 @@
-import { ProductList } from "components/ProductList/ProductList";
+import { useSearchParams } from "react-router-dom";
+import ProductList from "components/ProductList/ProductList";
+import { SearchBox } from "components/SearchBox/SearchBox";
 import { getProducts } from "fakeAPI";
 
-export const Products = () => {
+ const Products = () => {
   const products = getProducts();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const productName = searchParams.get("name") ?? "";
+
+  const visibleProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(productName.toLowerCase())
+  );
+
+  const updateQueryString = (name) => {
+    const nextParams = name !== "" ? { name } : {};
+    setSearchParams(nextParams);
+  };
+
   return (
     <main>
-      <ProductList products={products} />
+      <SearchBox value={productName} onChange={updateQueryString} />
+      <ProductList products={visibleProducts} />
     </main>
   );
 };
 
-export default Products;
+export default Products
